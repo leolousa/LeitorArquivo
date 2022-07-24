f0b245ea327580c839fcbc084c67c783166321d1
leolousa <leolousa@hotmail.com>
2022-07-24 02:21:45 -0300

    Versão 4.0 lendo arquivo CSV e instanciando uma lista de objetos

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,14 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import application.model.Product;
 
 /**
  * Leitura de arquivo Texto
- * v3.0
+ * v4.0
  * @author Leonardo
  *
  */
@@ -15,17 +19,35 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "C:\\temp\\in.txt";
+		
+		List<Product> produtos = new ArrayList<Product>();
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
+			line = br.readLine();
+			
 			while (line != null) {
-				System.out.println(line);
+				String[] vetor = line.split(",");
+				
+				String name = vetor[0];
+				Double price = Double.parseDouble(vetor[1]);
+				Integer qtd = Integer.parseInt(vetor[2]);
+				
+				Product prod = new Product(name, price, qtd);
+				
+				produtos.add(prod);
+				
 				line = br.readLine();
 			}
 		} 
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		} 
+		
+		System.out.println("PRODUCTS:");
+		for(Product p : produtos) {
+			System.out.println(p);
+		}
 	}
 
 }
